af889f770acfb5360f5595fc2f8c47a87dba26a1
Quentin Gibson <quentingibson94@gmail.com>
2024-01-24 09:45:02 -0500

    add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../app/**/*.stories.tsx"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-themes",
+  ],
+  features: { experimentalRSC: true },
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+};
+export default config;
---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Press_Start_2P, Open_Sans } from "next/font/google";
+
+export const Press_Start = Press_Start_2P({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const sans = Open_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
