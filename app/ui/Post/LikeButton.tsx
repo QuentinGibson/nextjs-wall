16e8b0e163beba8ee8371d4d13df1d638775981a
Quentin Gibson <quentingibson94@gmail.com>
2024-02-10 05:46:09 +0000

    add optimistic like button

--- app/ui/Post/LikeButton.tsx
+++ app/ui/Post/LikeButton.tsx
@@ -4,6 +4,7 @@ import { HeartIcon as HeartIconOutline } from "@heroicons/react/24/outline";
 import { HeartIcon as HeartIconSolid } from "@heroicons/react/24/solid";
 import { toggleLike } from "@/app/lib/actions";
 import { useFormState } from "react-dom";
+import { useOptimistic } from "react";
 
 export default function LikeButton({
   liked,
@@ -13,14 +14,23 @@ export default function LikeButton({
   postId: string;
 }) {
   const initialState = liked;
-  const [state, dispatch] = useFormState(toggleLike, initialState);
-
+  const [state, dispatch] = useFormState<boolean, FormData>(
+    toggleLike,
+    initialState
+  );
+  const [optimisticState, addOptimistic] = useOptimistic<boolean>(state);
   return (
-    <form action={dispatch}>
-      <input type="hidden" name="like" checked={state} />
-      <input type="hidden" name="postId" value={postId} />
+    <form
+      action={async (formData: FormData) => {
+        const like = !!formData.get("like");
+        formData.set("postId", postId);
+        addOptimistic((like) => !like);
+        dispatch(formData);
+      }}
+    >
+      <input type="hidden" name="like" checked={optimisticState} />
       <button className="like-button">
-        {state ? (
+        {optimisticState ? (
           <HeartIconSolid className="h-5 w-5" />
         ) : (
           <HeartIconOutline className="h-5 w-5" />
